4c67e7bcd1934f8314283fbb19bf7a3a46317fa8
Paulo Sousa <paulomiguel161616@hotmail.com>
2021-07-06 17:28:07 +0100

    changed Tree, foldTree and unfoldTree definition

--- src/DFAWordSearch.hs
+++ src/DFAWordSearch.hs
@@ -1,30 +1,23 @@
-{-# LANGUAGE DeriveFoldable #-}
-{-# LANGUAGE FlexibleContexts #-}
-
 module DFAWordSearch where
 
 import Language.HaLex.Dfa (Dfa (..))
 import Language.HaLex.Minimize (minimizeDfa)
 
-data LOL a = LOL [a]
-   deriving Eq
+data LOL a = LOL [a] !Int
+   deriving (Eq, Show)
 instance Ord a => Ord (LOL a) where
-   LOL x1 <= LOL x2 = (length x1, x1) <= (length x2, x2)
-instance Show a => Show (LOL a) where
-   show (LOL a) = show a
+   LOL x1 n1 <= LOL x2 n2 = (n1, x1) <= (n2, x2)
 
-data Tree t = Leaf | Branch t [Tree t]
+data Tree t = Branch t [Tree t]
 
-foldTree :: (b -> a -> a) -> (a -> a -> a) -> a -> Tree b -> a
-foldTree g h c (Branch x xs) = g x (foldForest g h c xs)
-foldTree g h c Leaf          = c
+foldTree :: (b -> a -> a) -> ([a] -> a) -> Tree b -> a
+foldTree f g (Branch x xs) = f x (foldForest f g xs)
 
-foldForest :: (b -> a -> a) -> (a -> a -> a) -> a -> [Tree b] -> a
-foldForest g h c xs          = foldr h c (map (foldTree g h c) xs)
+foldForest :: (b -> a -> a) -> ([a] -> a) -> [Tree b] -> a
+foldForest f g xs          = g (map (foldTree f g) xs)
 
-unfoldTree :: (a -> Bool) -> (a -> b) -> (a -> [a]) -> a -> Tree b
-unfoldTree c d t e = if (c e) then Leaf
-                              else Branch (d e) (map (unfoldTree c d t) (t e))
+unfoldTree :: (a -> b) -> (a -> [a]) -> a -> Tree b
+unfoldTree d t e = Branch (d e) (map (unfoldTree d t) (t e))
 
 infixl 6 \/
 (\/) :: Ord a => [a] -> [a] -> [a]
@@ -36,16 +29,24 @@ xs@(x:xt) \/ ys@(y:yt) = case compare x y of
    GT -> y : xs\/yt
 
 wordSearch :: (Eq sy, Eq st, Ord sy, Ord st) => Dfa st sy -> [[sy]]
-wordSearch = map (\(LOL s) -> s) . hyloWordSearch
+wordSearch = map (\(LOL s n) -> s) . hyloWordSearch
 
 hyloWordSearch :: (Eq sy, Eq st, Ord sy, Ord st) => Dfa st sy -> [LOL sy]
-hyloWordSearch fulldfa = map fst
-                         $ filter snd
-                         $ foldTree (:) (\/) []
-                         $ unfoldTree (\(_      , st) -> elem st trash)
-                                      (\(LOL str, st) -> (LOL str, elem st fin))
-                                      (\(LOL str, st) -> [(LOL (str ++ [ch]), trans st ch) | ch <- voc])
-                         (LOL [], init)
-    where
-        dfa@(Dfa voc sts init fin trans) = minimizeDfa fulldfa
-        trash = [ x | x <- sts, not (elem x fin), and [x == (trans x v) | v <- voc]]
+hyloWordSearch fulldfa = 
+    map fst
+    $ filter snd
+    $ foldTree (:) (foldr (\/) [])
+    $ unfoldTree 
+        (\(LOL str n, st) -> (LOL str n, elem st fin))
+        (\(LOL str n, st) -> 
+            [(LOL (str ++ [ch]) (n+1), trans st ch) 
+                | ch <- alpha
+                , not $ elem (trans st ch) dead])
+    (LOL [] 0, init)
+        where
+            dfa@(Dfa alpha sts init fin trans) = 
+                minimizeDfa fulldfa
+            dead = 
+                [ x | x <- sts 
+                    , not (elem x fin)
+                    , and [x == (trans x ch) | ch <- alpha]]
